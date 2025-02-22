84bcee0ed658aeb1262c8df34acd4f5914c55a9f
Onyinyechi.Chukwuma <yin.chukwuma@mrmoco.com>
2025-02-22 15:22:53 +0000

    Update About Page

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-portfolio-template/',
+  base: '/portfolio/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
